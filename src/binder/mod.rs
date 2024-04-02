c09745feea35e4eff51e3bb1521dafb9632f10c0
Ani <anirudh2000@gmail.com>
2024-04-01 17:07:58 -0700

    functional ast to substrait for query 5

--- src/binder/mod.rs
+++ src/binder/mod.rs
@@ -1,12 +1,17 @@
+use std::vec;
+
 use substrait::proto::{
     self, aggregate_rel::Grouping, expression::{
-        self, field_reference::{self, ReferenceType}, reference_segment::StructField, FieldReference,
-        ReferenceSegment, RexType, ScalarFunction,
+        self,
+        field_reference::ReferenceType,
+        reference_segment::StructField,
+        FieldReference, ReferenceSegment, RexType, ScalarFunction,
     }, extensions::{
         self,
         simple_extension_declaration::{ExtensionFunction, MappingType},
         SimpleExtensionDeclaration, SimpleExtensionUri,
-    }, function_argument::ArgType, plan_rel::RelType, read_rel, r#type::{self, Kind, Struct}, Expression, FunctionArgument, NamedStruct, Plan, PlanRel, Rel, RelRoot, Type
+    }, function_argument::ArgType, plan_rel::RelType, read_rel, r#type::{self, Kind, Struct}, Expression, FunctionArgument, NamedObjectWrite, NamedStruct, Plan, PlanRel, Rel, RelRoot, Type,
+    ddl_rel
 };
 
 use crate::parser::ast::*;
@@ -31,17 +36,16 @@ fn select_statement_to_substrait(
 
     //if the select statement has a where clause
     if let Some(where_clause) = &select.where_clause {
-        if let Condition::GreaterThan {
-            column,
-            value: _,
-        } = &where_clause.condition
-        {
+        if let Condition::GreaterThan { column, value: _ } = &where_clause.condition {
             //putting column in a list of columns -> list of strings -> string -> field index of string in base schema
             let column_slice = std::slice::from_ref(column);
             let field_names = construct_column_names(column_slice);
-            let field_name = field_names.get(0).map(|name| name.as_str()).unwrap_or_default();
+            let field_name = field_names
+                .get(0)
+                .map(|name| name.as_str())
+                .unwrap_or_default();
             let field_index = get_field_index_in_schema(&get_hits_base_schema(), field_name)
-            .expect("Column name not found in schema"); // This should be handled more gracefully
+                .expect("Column name not found in schema"); // This should be handled more gracefully
 
             let condition_expr = Expression {
                     rex_type: Some(expression::RexType::ScalarFunction(ScalarFunction {
@@ -100,7 +104,7 @@ fn select_statement_to_substrait(
             .into_iter()
             .map(|name| {
                 let field_index = get_field_index_in_schema(&get_hits_base_schema(), &name)
-                .expect("Field name not found in schema");
+                    .expect("Field name not found in schema");
                 Expression {
                     rex_type: Some(RexType::Selection(Box::new(FieldReference {
                         reference_type: Some(ReferenceType::DirectReference(ReferenceSegment {
@@ -199,18 +203,78 @@ fn get_hits_base_schema() -> NamedStruct {
         ],
         r#struct: Some(Struct {
             types: vec![
-                Type { kind: Some(Kind::I64(r#type::I64 { nullability: 1, ..Default::default() })) }, // Assuming this represents an "id" field.
-                Type { kind: Some(Kind::Timestamp(r#type::Timestamp { nullability: 1, ..Default::default() })) }, // Assuming this represents a "timestamp" field.
-                Type { kind: Some(Kind::I64(r#type::I64 { nullability: 1, ..Default::default() })) }, // Assuming this represents a "user_id" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents an "event_type" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents a "url" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents a "session_id" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents a "user_agent" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents a "country" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents an "ip" field.
-                Type { kind: Some(Kind::String(r#type::String { nullability: 1, ..Default::default() })) }, // Assuming this represents a "referrer" field.
-                Type { kind: Some(Kind::I64(r#type::I64 { nullability: 1, ..Default::default() })) }, // Assuming this represents a "clicks" field.
-                Type { kind: Some(Kind::I64(r#type::I64 { nullability: 1, ..Default::default() })) }, // Assuming this represents an "impressions" field.
+                Type {
+                    kind: Some(Kind::I64(r#type::I64 {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents an "id" field.
+                Type {
+                    kind: Some(Kind::Timestamp(r#type::Timestamp {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "timestamp" field.
+                Type {
+                    kind: Some(Kind::I64(r#type::I64 {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "user_id" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents an "event_type" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "url" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "session_id" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "user_agent" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "country" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents an "ip" field.
+                Type {
+                    kind: Some(Kind::String(r#type::String {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "referrer" field.
+                Type {
+                    kind: Some(Kind::I64(r#type::I64 {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents a "clicks" field.
+                Type {
+                    kind: Some(Kind::I64(r#type::I64 {
+                        nullability: 1,
+                        ..Default::default()
+                    })),
+                }, // Assuming this represents an "impressions" field.
                 Type {
                     kind: Some(Kind::Decimal(r#type::Decimal {
                         precision: 10,
@@ -221,13 +285,89 @@ fn get_hits_base_schema() -> NamedStruct {
                 }, // Assuming this represents a "revenue" field.
             ],
             type_variation_reference: 0, // Default value for simplicity
-            nullability: 1, // Assuming the whole struct is required
+            nullability: 1,              // Assuming the whole struct is required
         }),
     }
 }
 
 fn get_field_index_in_schema(schema: &NamedStruct, field_name: &str) -> Option<usize> {
     schema.names.iter().position(|name| name == field_name)
+}
+
+fn create_named_struct_from_columns(columns: &[ColumnDefinition]) -> NamedStruct {
+    let names = columns.iter().map(|col| col.name.clone()).collect();
+    let types = columns.iter().map(|col| {
+        match col.data_type {
+            DataType::Int | DataType::UInt64 => {
+                Type { 
+                    kind: Some(Kind::I64(substrait::proto::r#type::I64::default())) // Assuming I64 has a default implementation
+                }
+            },
+            DataType::String => {
+                Type { 
+                    kind: Some(Kind::String(substrait::proto::r#type::String::default())) // Assuming String has a default implementation
+                }
+            },
+            DataType::DateTime => {
+                Type { 
+                    kind: Some(Kind::Timestamp(substrait::proto::r#type::Timestamp::default())) // Assuming Timestamp has a default implementation
+                }
+            },
+            DataType::Decimal(precision, scale) => {
+                Type { 
+                    kind: Some(Kind::Decimal(substrait::proto::r#type::Decimal { 
+                        precision: precision as i32, 
+                        scale: scale as i32,
+                        ..substrait::proto::r#type::Decimal::default() // Assuming additional fields can be filled with defaults
+                    })) 
+                }
+            },
+            // Handle other data types
+        }
+    }).collect();
+
+    NamedStruct {
+        names,
+        r#struct: Some(Struct {
+            types,
+            type_variation_reference: 0, // Default for simplicity
+            nullability: 1, // Assuming the whole struct is required
+        }),
+    }
+}
+
+fn create_table_statement_to_substrait(
+    create: &CreateTableStatement,
+) -> Result<PlanRel, Box<dyn std::error::Error>> {
+
+
+    let ddl_rel = Rel {
+        rel_type: Some(proto::rel::RelType::Ddl(Box::new(proto::DdlRel {
+            table_schema: Some(create_named_struct_from_columns(&create.columns)),
+            object: 1, 
+            op: 1, 
+            write_type: Some(ddl_rel::WriteType::NamedObject(NamedObjectWrite {
+                names: vec![create.table_name.name.clone()],
+                advanced_extension: None
+            })),
+            ..Default::default()
+        })))
+    };
+
+      // Construct the root relation
+      let root_rel = RelRoot {
+        input: Some(ddl_rel),
+        ..Default::default()
+    };
+
+    let rel_type = RelType::Root(root_rel);
+
+    // Construct the plan with the root relation
+    let plan_rel = PlanRel {
+        rel_type: Some(rel_type),
+    };
+
+    Ok(plan_rel)
 }
 
 //function to convert any statement into a substrait plan based on the type of Statement it is
@@ -270,13 +410,13 @@ pub fn ast_to_substrait_plan(ast: &Statement) -> Result<Plan, Box<dyn std::error
                     name: "gt".to_string(),
                 }))
             },
-        }
+        },
     ];
 
     match ast {
         Statement::Select(select_stmt) => {
             let plan_rel = select_statement_to_substrait(select_stmt)?;
-            
+
             // Construct the Plan with the RelRoot
             let plan = Plan {
                 relations: vec![plan_rel], // Add the RelRoot to the Plan
@@ -285,6 +425,14 @@ pub fn ast_to_substrait_plan(ast: &Statement) -> Result<Plan, Box<dyn std::error
                 ..Default::default()
             };
             Ok(plan) // Return the constructed Plan
+        }
+        Statement::CreateTable(create_stmt) => {
+            let plan_rel = create_table_statement_to_substrait(create_stmt)?;
+            let plan = Plan {
+                relations: vec![plan_rel],
+                ..Default::default()
+            };
+            Ok(plan)
         }
         // Handle other Statement variants
         _ => Err("Unsupported AST node type".into()),
